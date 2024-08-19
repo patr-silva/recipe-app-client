cba736aca1b0e078a9a3c9e9db9d0b3c151e32ce
Patrícia Silva <98959396+patr-silva@users.noreply.github.com>
2024-08-19 22:08:38 +0100

    fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -198,7 +198,7 @@ const Navbar = () => {
                       to='/auth/login'
                       className='-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-orange-300 hover:text-white'
                     >
-                      Log in
+                      Login
                     </Link>
                     <Link
                       to='/auth/register'
